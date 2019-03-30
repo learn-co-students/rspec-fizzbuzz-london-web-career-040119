332750bd691cc6e6c888f792c7836d9fb7a5b8a0
Elle Valentine <elle.valentine@hotmail.com>
2019-03-30 10:08:59 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(num)
+  
+    
+    if ((num.Float % 3 == 0) && (num.Float % 5 == 0))
+      return "FizzBuzz"
+      
+    elsif (num.Float % 3 == 0)
+      return "Fizz"
+      
+    elsif (num.Float % 5 == 0)
+      return "Buzz"
+      
+    else
+      return "nil"
+      
+    end
+  end
+end
